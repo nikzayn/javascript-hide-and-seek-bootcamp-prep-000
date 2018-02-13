964018f9b8bb3c401920107073234ddee2345291
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-13 18:30:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+function deepestChild(){
+  var child = document.getElementById("grand-node").querySelectorAll("div");
+  var deep;
+  for(let i = 0; i < child.length-1; i++){
+    deep = child[i].querySelector("div");
+  }
+  return deep;
+}
+function increaseRankBy(n){
+  
+}
